3703b934194fc5e4c19d66eb6d579141ec44eae7
WangXinzuo <wangxinzuo.uestc@gmail.com>
2016-11-26 17:09:27 -0600

    classifier 1

--- classifier_1.py
+++ classifier_1.py
@@ -1,6 +1,9 @@
 import pickle
 import sklearn
-from sklearn import svm # this is an example of using SVM
+from sklearn import svm, metrics# this is an example of using SVM
+import matplotlib.pyplot as plt
+import numpy as np
+from mnist import load_mnist
 
 def preprocess(images):
     #this function is suggested to help build your classifier. 
@@ -12,7 +15,7 @@ def build_classifier(images, labels):
     #this will actually build the classifier. In general, it
     #will call something from sklearn to build it, and it must
     #return the output of sklearn. Right now it does nothing.
-    classifier = svm.SVC()
+    classifier = svm.SVC(gamma=0.001)
     classifier.fit(images, labels)
     return classifier
 
@@ -28,7 +31,8 @@ def save_classifier(classifier, training_set, training_labels):
 
 def classify(images, classifier):
     #runs the classifier on a set of images. 
-    return classifier.predict(images)
+    clsres = classifier.predict(images)
+    return clsres
 
 def error_measure(predicted, actual):
     return np.count_nonzero(abs(predicted - actual))/float(len(predicted))
@@ -36,7 +40,7 @@ def error_measure(predicted, actual):
 if __name__ == "__main__":
 
     # Code for loading data
-    
+    images, labels = load_mnist(digits=range(0, 10), path='.')
     # preprocessing
     images = preprocess(images)
     
@@ -50,6 +54,13 @@ if __name__ == "__main__":
     #build_classifier is a function that takes in training data and outputs an sklearn classifier.
     classifier = build_classifier(training_set, training_labels)
     save_classifier(classifier, training_set, training_labels)
-    classifier = pickle.load(open('classifier'))
+    classifier = pickle.load(open('classifier_1.p'))
     predicted = classify(testing_set, classifier)
+
+    print("Classification report for classifier %s:\n%s\n"
+      % (classifier, metrics.classification_report(testing_labels, predicted)))
+    print("Confusion matrix:\n%s" % metrics.confusion_matrix(testing_labels, predicted))
+
+    for i, item in enumerate(testing_labels):
+        print predicted[i], '\t', testing_labels[i]
     print error_measure(predicted, testing_labels)
